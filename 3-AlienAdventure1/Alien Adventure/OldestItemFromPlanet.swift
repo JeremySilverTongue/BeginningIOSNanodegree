01b4252b00925050c3c7279a46a8ef446a85f3a1
Jeremy Silver <jeremy@udacity.com>
2016-07-08 17:09:34 -0700

    Finish oldestItemFromPlanet

--- 3-AlienAdventure1/Alien Adventure/OldestItemFromPlanet.swift
+++ 3-AlienAdventure1/Alien Adventure/OldestItemFromPlanet.swift
@@ -9,9 +9,26 @@
 extension Hero {
     
     func oldestItemFromPlanet(inventory: [UDItem], planet: String) -> UDItem? {
-        return nil
+        
+        let fromPlanet = itemsFromPlanet(inventory, planet: planet)
+        
+        var minAge = Int.min
+        var oldest: UDItem?
+        
+        for item in fromPlanet{
+            let ageData = item.historicalData["CarbonAge"]
+            if let age = ageData as? Int {
+                if age > minAge {
+                    minAge = age
+                    oldest = item
+                }
+            }
+        }
+        return oldest
     }
     
 }
+
+
 
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 2"
