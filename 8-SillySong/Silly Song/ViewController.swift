d2aceab60097e460570f4e6a2ee480197ddc8ba0
Jeremy Silver <jeremy@udacity.com>
2016-07-12 12:09:33 -0700

    Initial project files for SillySong

---
+++ 8-SillySong/Silly Song/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Silly Song
+//
+//  Created by Jeremy Silver on 7/12/16.
+//  Copyright © 2016 Jeremy Silver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
