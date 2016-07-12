3569830865c514cc761262d389f811a0738283f6
Jeremy Silver <jeremy@udacity.com>
2016-07-12 14:40:25 -0700

    Finish SillySong

--- 8-SillySong/Silly Song/ViewController.swift
+++ 8-SillySong/Silly Song/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
+    
+    @IBAction func reset(sender: AnyObject) {
+        nameField.text = ""
+        lyricsView.text = ""
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func displayLyrics(sender: AnyObject) {
+        lyricsView.text = lyricsForName(nameField.text!)
     }
-
-
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func shortNameForName(name: String) -> String {
+        let lowercaseName = name.lowercaseString
+        let vowelSet = NSCharacterSet(charactersInString: "aeiou")
+        if let firstVowelRange = lowercaseName.rangeOfCharacterFromSet(vowelSet) {
+            return lowercaseName.substringFromIndex(firstVowelRange.startIndex)
+        }
+        return lowercaseName
+    }
+    
+    func lyricsForName(name: String) -> String {
+        let shortName = shortNameForName(name)
+        
+        return "\(name), \(name), Bo B\(shortName)\n" +
+            "Banana Fana Fo F\(shortName)\n" +
+            "Me My Mo M\(shortName)\n" +
+            "\(name)"
+    }
+    
 }
 
